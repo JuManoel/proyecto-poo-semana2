ccf50ac89933c58f316d7141c56a2fb212a720fe
JuManoel <gmjm2004@gmail.com>
2024-02-12 10:41:22 -0300

    implementacion del metodo readInt y documentacion

--- main/src/main/java/edu/prog2/helpers/Keyboard.java
+++ main/src/main/java/edu/prog2/helpers/Keyboard.java
@@ -13,11 +13,34 @@ import java.util.Scanner;
 public class Keyboard {
 
     private static Console con = System.console();//objeto da clase console, serve para ler strings e SENHAS
-    public static Scanner sc = new Scanner(con.reader()).useDelimiter("[\n]+|[\r\n]+");
+    public static Scanner sc = new Scanner(con.reader()).useDelimiter("[\n]+|[\r\n]+");//con.reader y useDelimiter es los mismo que el System.in
+    //"[\n]+[\r\n] entiende qualquier salto de linea. enter"
 
     public static String readString(String message) {
+        //lee apenas Strings. muestra una mensage
         System.out.print(message);
         return sc.nextLine();
+    }
+
+    public static int readInt(String message) {
+        //leer ints
+        boolean ok;//valida si el valor de la entrada es correcto
+        int value = Integer.MIN_VALUE;//valor minimo del inteiro
+        System.out.print(message);//imprime un mensage
+    
+        do {
+            try {
+                ok = true;//la entrada esta bien
+                value = sc.nextInt(); //recibe el valor
+            } catch (InputMismatchException e) {//caso tenga un error de input
+                ok = false;//no esta ok
+                System.out.print(">> Valor erróneo. " + message);//muestra que el valor ingresado es malo
+            } finally {
+                sc.nextLine();//salto de linea
+            }
+        } while (!ok);//si la entrada es ok. entonce termina el ciclo. si no. repite
+    
+        return value;//retorna el valor
     }
 
 }
